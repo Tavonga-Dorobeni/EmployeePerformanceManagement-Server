74beb8050a31df3e497e15fb7c9c13f5b111e781
ShalomMuleiwa <muleiwashalom20@gmail.com>
2024-05-02 11:15:44 +0200

    'Updates'

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  HOST: process.env.MYSQL_HOST || "localhost",
-  USER: process.env.MYSQL_USER || "root",
-  PASSWORD: process.env.MYSQL_PASS || "shalom0507!",
-  DB: process.env.MYSQL_DB || "epms",
+  // HOST: process.env.MYSQL_HOST || "localhost",
+  // USER: process.env.MYSQL_USER || "root",
+  // PASSWORD: process.env.MYSQL_PASS || "shalom0507!",
+  // DB: process.env.MYSQL_DB || "epms",
 
-  // HOST: "194.233.164.50",
-  // USER: "root",
-  // PASSWORD: "MySQLR00tP@55202two",
-  // DB: "callibration_automation",
-  // PORT: "32768",
+  HOST: "194.233.164.50",
+  USER: "root",
+  PASSWORD: "MySQLR00tP@55202two",
+  DB: "epms",
+  PORT: "32768",
 
   dialect: "mysql",
   pool: {
